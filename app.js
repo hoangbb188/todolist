250eb77405d5422e130481788686202ab3fdd6a7
hoangbb188 <132394743+hoangbb188@users.noreply.github.com>
2023-05-05 00:12:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -20,28 +20,7 @@
     })();
     /******/
   })();
-  /******/
-  /******/ /* webpack/runtime/publicPath */
-  /******/ (() => {
-    /******/ var scriptUrl;
-    /******/ if (__webpack_require__.g.importScripts)
-      scriptUrl = __webpack_require__.g.location + "";
-    /******/ var document = __webpack_require__.g.document;
-    /******/ if (!scriptUrl && document) {
-      /******/ if (document.currentScript)
-        /******/ scriptUrl = document.currentScript.src;
-      /******/ if (!scriptUrl) {
-        /******/ var scripts = document.getElementsByTagName("script");
-        /******/ if (scripts.length)
-          scriptUrl = scripts[scripts.length - 1].src;
-        /******/
-      }
-      /******/
-    }
-  })();
-
-
-  // CONCATENATED MODULE: ./src/modules/tasks.js
+ 
   const tasksHandler = {
     items: [],
     addTask: function (task) {
